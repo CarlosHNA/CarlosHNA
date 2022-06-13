ddf3ac5935952c950bc14d2b4f55a0e04ae639fd
CarlosHNA <103291876+CarlosHNA@users.noreply.github.com>
2022-06-13 15:01:48 -0300

    Add files via upload
    Alterações no arquivo game.js junto com o style.css para que quando ocorrer a ação de impacto do Sonic com o obstáculo imprima uma mensagem de game over.

--- Jogo Sonic Jump/game.js
+++ Jogo Sonic Jump/game.js
@@ -1,13 +1,31 @@
 const sonic = document.querySelector('.sonic');
 const eggman = document.querySelector('.eggman');
+const shadow = document.querySelector('.shadow');
 
 const jump = () => {
-    sonic.classList.add ('jump');
+  sonic.classList.add('jump');
 
-    setTimeout(() => {
-      sonic.classList.remove ('jump');    
-    }, 650);
+  setTimeout(() => {
+    sonic.classList.remove('jump');
+  }, 650);
 }
 
-document.addEventListener('keydown', jump);
+const loop = setInterval(() => {
+  const eggmanPosition = eggman.offsetLeft;
+  const sonicPosition = +window.getComputedStyle(sonic).bottom.replace('px', '');
+
+  if (eggmanPosition <= 240 && eggmanPosition > 0 && sonicPosition < 130) {
+
+    eggman.style.animation = 'none';
+    eggman.style.left = `${eggmanPosition}px`;
+
+    sonic.style.animation = 'none';
+    sonic.style.bottom = `${sonicPosition}px`;
+
+    sonic.src = "Sonic gameover.png"
+
+  }
+})
+document.addEventListener('keydown', jump)
+
 
